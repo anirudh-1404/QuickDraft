1330bf220b52e550aa980515d961b73497bed861
Anirudh Joshi <160882629+anirudh-1404@users.noreply.github.com>
2025-12-31 13:31:55 +0530

    Delete backend directory

--- backend/utils/authToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const genToken = async (id) => {
-  return await jwt.sign({ id: id }, process.env.SECRET_KEY, {
-    expiresIn: "2d",
-  });
-};
-
-export default genToken;
